f0c2b35b9ce039f29ef9f11430fc5a524162a95d
Esmaeil Chitgar <84287711+EsmaeilChitgar@users.noreply.github.com>
2023-07-20 14:44:13 +0330

    add main class for src and test

---
+++ src/main/java/com/echitgar/Jsonschema2PojoServiceApplication.java
@@ -0,0 +1,14 @@
+package com.echitgar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsonschema2PojoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Jsonschema2PojoServiceApplication.class, args);
+
+//    Message message = new Message().withMessage("My plain text message");
+//    System.out.println(message.toString());
+    }
+}
---
+++ src/test/java/com/echitgar/POJO2JsonServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.echitgar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class POJO2JsonServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
